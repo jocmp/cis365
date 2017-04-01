71c82b7cf371d032f41c7028ee66d2324608dcad
Josiah Campbell <josiahcampbell64@gmail.com>
2017-04-01 13:29:47 -0400

    Update zip files for data

--- term-project/convert_to_csv.rb
+++ term-project/convert_to_csv.rb
@@ -3,13 +3,14 @@
 CSV.open('tmp/all_attrs.csv', 'wb') do |csv|
   csv << Message.attribute_names
   Message.find_each do |message| # batches of 1000
-    csv << message.attributes.values
+    csv << [message.weight,	message.humidity, message.temperature,
+            message.occurance_time, message.latitude, message.longitude]
   end
 end
 
 CSV.open('tmp/just_weight.csv', 'wb') do |csv|
   csv << ['weight']
   Message.find_each do |message|
-    csv << [message.weight]
+    csv << [message.weight, message.occurance_time]
   end
 end
